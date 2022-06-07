7c6b630a10088ac4100b246fc3194453f6cac68d
kantrax <diegogccantarell@gmail.com>
2022-06-07 17:09:15 -0500

    13 Updating the file 'spiderman.test.js', adding new test for the function getInfo()

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -11,4 +11,11 @@ describe("Unit Test for Spiderman class", () => {
     expect(andrewGarfield.studio).toBe("Sony")
 
   });
+
+  test('2) Use the method getInfo()',() =>{
+  const tomHolland = new Spiderman("Spiderman Marvel", 25,"Tom Holland", 5,"Marvel studio")
+  expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+  });
+
+
 })
